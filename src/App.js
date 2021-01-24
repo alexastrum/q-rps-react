e4fac9210057590c7da32b712ec539a559177b62
Alex Astrum <alexastrum@gmail.com>
2021-01-24 09:59:37 -0500

    Add step 4

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ export default function App() {
       <h1>Rock, paper, scissors game</h1>
       <ol>
         <li>Implement game logic in service.js</li>
-        <li>Implement UI for player to choose their move</li>
-        <li>Implement UI to show the game results</li>
+        <li>Implement primitive UI for player to choose their move</li>
+        <li>Implement primitive UI to show the game results</li>
+        <li>
+          Make UI look nice (use IconButton and provided png files to select the
+          move; show computer move in a Card, show game result in an Alert)
+        </li>
       </ol>
     </div>
   );
